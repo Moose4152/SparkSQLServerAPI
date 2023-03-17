b2cafed1185875e8df7b8f80156e06fb5e398c60
MayankSinghRana <mayanksinghrana@MayankSinghRanas-MacBook-Air.local>
2023-03-17 19:22:11 +0530

    Update Thrift file

---
+++ src/main/scala/processor/Engine.scala
@@ -0,0 +1,63 @@
+package processor
+
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.SQLContext
+
+object Engine {
+  val sparkConf = new SparkConf()
+
+  val localSparkConf = new SparkConf().setAppName("SparkAPIServer").setMaster("local[*]")
+  var currentActiveSQLContext:SQLContext = null
+  var currentActiveSparkContext:SparkContext = null
+
+  val sparkContext = () => this.synchronized {
+    if (currentActiveSparkContext != null) {
+      if (currentActiveSparkContext.isStopped) {
+        println("SparkContext Crashed. Restarting Spark Context")
+        currentActiveSparkContext.stop()
+        Thread.sleep(60000)
+        currentActiveSparkContext = {
+          val sc = new SparkContext(sparkConf)
+          sc.setLocalProperty("spark.scheduler.pool", "default")
+          import java.util.concurrent.Executors
+
+          import concurrent.ExecutionContext
+          val executorService = Executors.newFixedThreadPool(50)
+          implicit val ec = ExecutionContext.fromExecutorService(executorService)
+          sc
+        }
+      }
+    }
+    else {
+      currentActiveSparkContext = {
+        val sc = new SparkContext(sparkConf)
+        sc.setLocalProperty("spark.scheduler.pool", "default")
+        import java.util.concurrent.Executors
+        import concurrent.ExecutionContext
+        val executorService = Executors.newFixedThreadPool(50)
+        implicit val ec = ExecutionContext.fromExecutorService(executorService)
+        sc
+      }
+    }
+    currentActiveSparkContext
+  }
+
+  val sqlContext = () => this.synchronized {
+    if (currentActiveSQLContext != null) {
+      if (currentActiveSQLContext.sparkContext.isStopped) {
+        currentActiveSQLContext = {
+          val tempSQLContext = new SQLContext(sparkContext())
+          tempSQLContext
+        }
+      }
+    }
+    else {
+      currentActiveSQLContext = {
+        val tempSQLContext = new SQLContext(sparkContext())
+        tempSQLContext
+      }
+    }
+    currentActiveSQLContext
+  }
+
+}
--- src/main/scala/server/QueryHandler.scala
+++ src/main/scala/server/QueryHandler.scala
@@ -2,28 +2,43 @@ package server
 
 import com.github.msr.{SparkAPI, TResults}
 import org.apache.spark.sql.SparkSession
+import processor.Engine
 
 class QueryHandler extends SparkAPI.Iface{
-  val spark = SparkSession.builder().master("local[6]").appName("SparkAPIBackend").getOrCreate()
+//  val spark = SparkSession.builder().master("local[6]").appName("SparkAPIBackend").getOrCreate()
   val basePath = "/Users/mayanksinghrana/Desktop/TdigestExp/Functional/rollup1/"
   override def exectureCommand(sql_statment: String): TResults = {
-    val df = spark.read.parquet(basePath)
-    df.createOrReplaceTempView("RawTable")
-    val dfTemp = spark.sql(sql_statment)
-    dfTemp.show()
-    val result = new TResults()
-    val collectedResult = dfTemp.collect()
-    val res = collectedResult.toSeq.map(_.toSeq.map(x => {
-      try{
-        x.toString
-      } catch {
-        case _ => "0"
-      }
-    }))
-    result.setResults(scala.collection.JavaConverters.seqAsJavaList(res.map(m => scala.collection.JavaConverters.seqAsJavaList(m))))
-    val schema = dfTemp.columns.toSeq
-    result.setSchema(scala.collection.JavaConverters.seqAsJavaList(schema))
-    result
+    try{
+      val sqlContext = Engine.sqlContext()
+      val df = sqlContext.read.parquet(basePath)
+      df.createOrReplaceTempView("RawTable")
+      val dfTemp = sqlContext.sql(sql_statment)
+      dfTemp.show()
+      val result = new TResults()
+      val collectedResult = dfTemp.collect()
+      val res: Seq[Seq[String]] = collectedResult.toSeq.map(_.toSeq.map(x => {
+        try{
+          x.toString
+        } catch {
+          case _ => "0"
+        }
+      }))
+      result.setResults(scala.collection.JavaConverters.seqAsJavaList(res.map(m => scala.collection.JavaConverters.seqAsJavaList(m))))
+      val schema: Seq[String] = dfTemp.columns.toSeq
+      result.setSchema(scala.collection.JavaConverters.seqAsJavaList(schema))
+      result
+    } catch {
+      case e:Exception =>
+        val messag = e.getMessage
+        val result = new TResults()
+        val res = Seq(Seq("d1","e"),Seq("has","iad"))
+        result.setResults(scala.collection.JavaConverters.seqAsJavaList(res.map(m => scala.collection.JavaConverters.seqAsJavaList(m))))
+        val schema = Seq("asd")
+        result.setSchema(scala.collection.JavaConverters.seqAsJavaList(schema))
+        result.setErrorMessage(messag)
+        result
+    }
+
   }
 
   override def readFile(pathOfFile: String): String = {
